10eb2e88f2fec79b580cf0d9cd7c261785ebffd4
Mateus <mateuslannes100@gmail.com>
2023-05-05 13:57:25 +0000

    Tratamento de exceção para o Menu, evitando receber qualquer numero alem do int

--- projeto_Bolao/Menu.java
+++ projeto_Bolao/Menu.java
@@ -1,5 +1,6 @@
 package projeto_Bolao;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Menu {
@@ -12,14 +13,43 @@ public class Menu {
     } 
 
     public static int imprimeMenu() {
+        boolean continua = true;
+        Scanner ler = new Scanner(System.in); 
+
+        while (continua) {
+            try {
+
+                System.out.println();
+                System.out.println("Escolha uma das opcoes");
+                System.out.println("1) Cadastrar jogador");
+                System.out.println("2) Cadastrar aposta");
+                System.out.println("3) Inserir sorteio e listar vencedores");
+                System.out.println("4) Sair");
+                System.out.printf("Digite aqui a opcao do menu: ");
+                int i = ler.nextInt();
+
+                for (int j = 0; j < 20; j++) {
+                    System.out.println();
+                }
+                
+                continua = false;
+                return i;
+
+            } catch (InputMismatchException e) {
+
+                for (int j = 0; j < 20; j++) {
+                    System.out.println();
+                }
+                
+                
+                System.out.println("\u001B[45m ERRO! Numero invalido, digite um numero inteiro! \u001B[0m");
+                //"\u001B[45m" + texto + "\u001B[0m"
+                ler.nextLine();
+            }
+        }
         /* imprime as primeiras opcoes do menu */
-        System.out.println();
-        System.out.println("Escolha uma das opcoes");
-        System.out.println("1) Cadastrar jogador");
-        System.out.println("2) Cadastrar aposta");
-        System.out.println("3) Inserir sorteio e listar vencedores");
-        System.out.println("4) Sair");
-        System.out.printf("Digite aqui a opcao do menu: ");
+
+        
         int i = lerOpcao();
         for (int j = 0; j < 20; j++) {
             System.out.println();
